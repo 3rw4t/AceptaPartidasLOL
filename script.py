79e5f2cc8e30a7a17f23fb24b9de25e7f29e1110
Angle <45601608+3rw4t@users.noreply.github.com>
2019-12-10 04:07:38 -0300

    Create script.py

---
+++ script.py
@@ -0,0 +1,14 @@
+from pynput.mouse import Controller, Button
+import time
+import win32gui
+mouse = Controller()
+w=win32gui
+condition = True
+while condition:
+
+    if w.GetWindowText(w.GetForegroundWindow()) == "League of Legends":
+        time.sleep(3)
+        mouse.position = (636, 562)
+        mouse.click(Button.left, 1)
+        condition = False
+    
